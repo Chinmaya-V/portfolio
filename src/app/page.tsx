e8736733a4f0d2e523bd7cc2466f203ac587c0c8
Chinmaya-V <chinmayav04@gmail.com>
2025-05-12 00:19:28 +0530

    chore: add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { staatliches_font } from '@/utils/fonts'
+
 export default function Home() {
-  return <main></main>
+  return (
+    <div className="h-full w-full flex flex-col bg-amber-300">
+      <nav>Navbar</nav>
+
+      <main>
+        <div className={`${staatliches_font.className} text-6xl italic font-bold`}>Chinmaya V</div>
+      </main>
+    </div>
+  )
 }
---
+++ src/utils/fonts.tsx
@@ -0,0 +1,20 @@
+import { Righteous, Staatliches, Work_Sans } from 'next/font/google'
+
+export const staatliches_font = Staatliches({
+  weight: '400',
+  preload: true,
+  subsets: ['latin'],
+})
+
+export const righteous_font = Righteous({
+  weight: '400',
+  preload: true,
+  subsets: ['latin'],
+})
+
+export const worksans_font = Work_Sans({
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  preload: true,
+  subsets: ['latin'],
+  style: ['normal', 'italic'],
+})
