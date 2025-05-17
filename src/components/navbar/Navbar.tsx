a5b5a915eb27045df56170d7191bba6972d7e8fc
Chinmaya-V <chinmayav04@gmail.com>
2025-05-18 00:40:32 +0530

    refactor: remove icons completely

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,45 +1,36 @@
 import { jura_font } from '@/utils/fonts'
 import Image from 'next/image'
 import Link from 'next/link'
-import IconAbout from '../ui/icons/icon-about'
-import IconSkills from '../ui/icons/icon-skills'
-import IconProjects from '../ui/icons/icon-projects'
-import IconContact from '../ui/icons/icon-contact'
-import IconAchievements from '../ui/icons/icon-achievements'
 
 const NAV_ITEMS = [
-  { href: '/about', label: 'About', icon: <IconAbout /> },
+  { href: '/about', label: 'About' },
   {
     href: '#skills',
     label: 'Skills',
-    icon: <IconSkills />,
   },
   {
     href: '#projects',
     label: 'Projects',
-    icon: <IconProjects />,
   },
   {
     href: '#contact',
     label: 'Contact',
-    icon: <IconContact />,
   },
   {
     href: '#achievements',
     label: 'Achievements',
-    icon: <IconAchievements />,
   },
 ]
 
 const Navbar = () => {
   return (
     <nav
-      className={`fixed top-0 px-8 py-1 w-full h-12 z-50 bg-gradient-to-b from-zinc-100/20 to-transparent
+      className={`fixed top-0 px-8 py-1 w-full h-16 z-50 bg-gradient-to-b from-zinc-100/20 to-transparent
       flex items-center justify-center backdrop-blur-sm text-gray-200 text-lg ${jura_font.className}`}>
       <div className="flex items-center justify-between w-full max-w-5xl">
         <Link
           href="/"
-          className="flex items-center bg-zinc-400 rounded-full p-1 hover:bg-gray-100 transition-colors duration-300">
+          className="flex items-center bg-zinc-400 rounded-lg p-1 hover:bg-gray-100 transition-colors duration-300">
           <Image src="/logo.svg" alt="logo" width={40} height={40} priority />
         </Link>
 
@@ -48,17 +39,15 @@ const Navbar = () => {
             <Link
               key={label}
               href={href}
-              className={`flex justify-center items-center gap-2 hover:text-purple-400`}>
+              className={`icon-wrapper flex justify-center items-center gap-2 hover:text-purple-400 hover:underline underline-offset-2 `}>
               {label}
             </Link>
           ))}
         </div>
 
-        <Link
-          href="#resume"
-          className="hover:text-purple-300 bg-purple-500 rounded-2xl px-2 transition-colors duration-300">
+        <button className="not-sm:hidden hover:scale-105 font-semibold bg-purple-400 px-2 transition-transform duration-300">
           Resume
-        </Link>
+        </button>
       </div>
     </nav>
   )
