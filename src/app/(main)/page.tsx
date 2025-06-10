3c32992d9cfa401a048643552a0d0aeffcc9ff23
Chinmaya-V <chinmayav04@gmail.com>
2025-06-10 22:22:43 +0530

    feat: add animation

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import StyledLines from '@/components/ui/styled-lines'
 import { righteous_font, black_ops_one_font } from '@/utils/fonts'
+import { motion } from 'motion/react'
 
 export default function Home() {
 	return (
@@ -7,13 +9,25 @@ export default function Home() {
 			id="home"
 			className="flex flex-col w-full h-full min-h-screen overflow-hidden justify-start items-center pt-[10%] max-sm:pt-[30%]"
 		>
-			<div className={`w-fit text-8xl max-sm:text-6xl font-bold text-gray-300 ${black_ops_one_font.className}`}>
+			<motion.div
+				whileInView={{ opacity: 1, y: 0 }}
+				initial={{ opacity: 0.1, y: 50 }}
+				transition={{ duration: 0.5, ease: 'circIn', delay: 0.5 }}
+				viewport={{ once: true }}
+				className={`w-fit text-8xl max-sm:text-6xl font-bold text-gray-300 ${black_ops_one_font.className}`}
+			>
 				CHINMAYA V
-			</div>
+			</motion.div>
 			<StyledLines />
-			<div className={`w-fit text-4xl max-sm:text-3xl font-bold text-gray-300 ${righteous_font.className}`}>
+			<motion.div
+				whileInView={{ opacity: 1, y: 0 }}
+				initial={{ opacity: 0, y: -50 }}
+				transition={{ duration: 0.5, ease: 'circIn', delay: 0.7 }}
+				viewport={{ once: true }}
+				className={`w-fit text-4xl max-sm:text-3xl font-bold text-gray-300 ${righteous_font.className}`}
+			>
 				A Fullstack Developer.
-			</div>
+			</motion.div>
 		</section>
 	)
 }
