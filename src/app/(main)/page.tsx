04e73954c6b479d74e267574ead86fb9a44d245e
Chinmaya-V <chinmayav04@gmail.com>
2025-06-10 21:27:59 +0530

    feat: update main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import StyledLines from '@/components/ui/styled-lines'
 import { righteous_font, black_ops_one_font } from '@/utils/fonts'
 
 export default function Home() {
@@ -6,11 +7,12 @@ export default function Home() {
 			id="home"
 			className="flex flex-col w-full h-full min-h-screen overflow-hidden justify-start items-center pt-[10%] max-sm:pt-[30%]"
 		>
-			<div className={`w-fit text-8xl max-sm:text-5xl font-bold text-gray-300 ${black_ops_one_font.className}`}>
+			<div className={`w-fit text-8xl max-sm:text-6xl font-bold text-gray-300 ${black_ops_one_font.className}`}>
 				CHINMAYA V
 			</div>
+			<StyledLines />
 			<div className={`w-fit text-4xl max-sm:text-3xl font-bold text-gray-300 ${righteous_font.className}`}>
-				Fullstack Developer.
+				A Fullstack Developer.
 			</div>
 		</section>
 	)
